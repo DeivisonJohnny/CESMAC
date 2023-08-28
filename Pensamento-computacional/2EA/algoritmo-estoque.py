fb9fc7e25d69903e600d5bc8e6ffbcb0f00a9137
Deivison Johnny <deivisonjohnny@gmail.com>
2023-08-28 15:28:49 -0300

    Comentado sobre os passos da EA

--- Pensamento-computacional/2EA/algoritmo-estoque.py
+++ Pensamento-computacional/2EA/algoritmo-estoque.py
@@ -8,13 +8,12 @@ while True:
     if (nameP in estoqueP):
         print("O produto ja esta cadastrado\n")
     else:
-        print("Nome valido \n") 
         estoqueP.append(nameP)
         result.append('Nome do produto: ' + nameP)
         
         if (len(categorias) > 0):
             for i in range(len(categorias)):
-                print(categorias[i])
+                print("\n",categorias[i])
             cate = input("""
                 Qual categoria você gostaria de cadastrar este produto?
                 """).lower()
@@ -39,14 +38,54 @@ while True:
             categorias.append(newCate.lower())
             result.append(f"Categoria cadastrada: {newCate}")
             
+        # Passo 4° Estrutura de repetição    
+
         while True:
             try:
                 qtd = int(input('Digite qual o número do estoque inicial em unidade: ')) 
                 if(qtd < 0):
                     print("Digite um valor positivo, por favor!!!")
                 else:
-                    result.append(f"Seu estoque inicial é de: {qtd}")
-                    break
+                    if(qtd > 0):
+                        print(f"O valor digitado foi {qtd}")
+                        confQtd = input(f"Você confirma o valor inicial de {qtd} no estoque [y/x]").lower()
+                        # Passo 3° estrutura de decisão/condicionais
+                        if(confQtd == 'y'):
+                            break
+                        # Passo 3° estrutura de decisão/condicionais
+                        if(confQtd != 'y'):
+                            sm = input("Você gostaria de adicionar, ou retirar? [1 - adicionar, 2 - retirar]")
+                            # Passo 3° estrutura de decisão/condicionais
+                            if(sm == '1' or sm =='adicionar'):
+                                print(f"Este é o seu estoque digitado: {qtd}")
+                                try:
+                                    smd = int(input(f"Quanto voce gostaria de tirar {qtd} + "))
+
+                                    # Passo 2° Operações aritméticas
+
+                                    res = qtd + smd
+                                    result.append("Estoque Total", f"{res}")
+                                    print(f" O estoque inicial ficou {res}")
+                                    break
+                                except ValueError:
+                                    print("Digite numeros inteiros")
+                            else:
+                                print("Digite um valor válido")
+                            if(sm == '2' or sm =='retirar'): #continuar a partir daqui
+                                print(f"Este é o seu estoque digitado: {qtd}")
+                                try:
+                                    sb = int(input(f"Quanto voce gostaria de tirar {qtd} - "))
+
+                                    # Passo 2° Operações aritméticas
+
+                                    res = qtd - sb
+                                    result.append("Estoque Total " + f"{res}")
+                                    print(f"O estoque inicial ficou {res}")
+                                    break
+                                except ValueError:
+                                    print("Digite numeros inteiros")
+                            else:
+                                print("Digite um valor válido")
             except ValueError:
                 print("Valor invalido, tente novamente")
 
@@ -55,8 +94,7 @@ while True:
         print(f"""
                 {result[i]}
                 """)
-    print(categorias, estoqueP)
-    cont = input("Gostaria de continuar cadastrando? [y/x]")
+    cont = input("Gostaria de continuar cadastrando? [y/x]").lower()
     if(cont != 'y'):
         break
     os.system('cls || clean')
